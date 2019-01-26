3b4f81599deee4fe15ab86a632df1ca326afa0a9
visualstudio <visualstudio@122-PC27>
2019-01-25 19:43:07 -0500

    second

---
+++ New folder/C0730478/C0730478/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730478
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+        }
+    }
+}
